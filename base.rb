890cbf0966cc0bb2b4a351cdc347dad6686b968e
Brandon Bledsoe <brbledsoe.m@gmail.com>
2014-08-04 12:49:22 -0500

    set up base layout for MKS Code Chanllenge.

---
+++ base.rb
@@ -0,0 +1,28 @@
+class Person
+
+  def initialize
+  end
+
+end
+
+
+
+class Bank
+
+  def initialize
+
+  end
+
+  def open_account
+  end
+
+  def withdraw
+  end
+
+  def deposit
+  end
+
+  def transfer
+  end
+
+end
